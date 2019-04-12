5354c5a0fec11ab985e085c519acaca2bd7062c1
Steven <sbaudet@entreloups.com>
2019-04-12 10:58:39 +0200

    delete MobileVLCKit.framework

--- App.js
+++ App.js
@@ -1,49 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react'
+import Router from 'react-native-easy-router'
+import { Text, View } from 'react-native'
+import { VlcSimplePlayer } from 'react-native-yz-vlcplayer';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const First = ({ router }) => (
+    <View style={{ flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF', }}>
+      <Text onPress={() => router.push.Second({ name: 'John' })}>LECTURE</Text>
+    </View>
+);
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const Second = ({ router, name }) => (
+    <View style={{flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',}}>
+        <VlcSimplePlayer
+            ref={ref => this.vlCPlayerView = ref}
+            style={{position: 'absolute', height: '100%'}}
+            url={'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov'}
+            // videoAspectRatio={wp('100%') + ':' + hp('100%')}
+            autoAspectRatio={true}
+            isLive={true}
+            autoplay={true}
+            autoReloadLive={true}
+            initType={2}
+            initOptions={[
+                "--rtsp-tcp",
+                "--network-caching=" + 150,
+                "--rtsp-caching=" + 150,
+                "--no-stats",
+                "--tcp-caching=" + 150,
+                "--realrtsp-caching=" + 150,
+                "--verbose=4",
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+            ]}
+            hwDecoderEnabled={1}
+            hwDecoderForced={1}
+            mediaOptions={{
+                ':network-caching': 30,
+                ':live-caching': 30,
+            }}
+        />
+      <Text onPress={() => router.pop()}>RETOUR</Text>
+    </View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const routes = { First, Second };
+const App = () => <Router routes={routes} initialRoute="First" />;
+
+export default App
